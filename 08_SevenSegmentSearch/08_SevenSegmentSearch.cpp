49de78691761e5cb4631689c3347e8b0ee9827f4
Alexander Zabluda <azabluda@gmail.com>
2022-02-08 09:28:05 +0100

    08_SevenSegmentSearch part2

--- 08_SevenSegmentSearch/08_SevenSegmentSearch.cpp
+++ 08_SevenSegmentSearch/08_SevenSegmentSearch.cpp
@@ -1,19 +1,42 @@
 #include <iostream>
+#include <unordered_map>
 #include <unordered_set>
 #include <sstream>
 using namespace std;
 
 string input();
 
+static size_t common_chars(string const& s) {
+    return s.size() - unordered_set(begin(s), end(s)).size();
+}
+
 int main() {
-    size_t res = 0, idx = 0;
-    unordered_set<size_t> ez{ 2, 4, 3, 7 };
-    string str;
-    for (stringstream file(input()); getline(file, str); idx = 0)
-        for (stringstream line(str); getline(line, str, ' ');)
-            if (idx++ > 10)
-                res += ez.count(str.size());
-    cout << "Part 1: " << res << endl;
+    size_t res1 = 0, res2 = 0;
+    unordered_map<size_t, size_t> ez{ {2, 1}, {4, 4}, {3, 7}, {7, 8} };
+    string str, str1, str4;
+    for (stringstream file(input()); getline(file, str);) {
+        size_t out = 0, idx = 0;
+        for (stringstream line(str); getline(line, str, ' ');) {
+            size_t cnt = str.size(), val = ez[cnt];
+            if (idx++ <= 10) {
+                if (val == 1) str1 = str;
+                if (val == 4) str4 = str;
+                continue;
+            }
+            res1 += !!val;
+            size_t cnt1 = common_chars(str + str1),
+                   cnt4 = common_chars(str + str4);
+            val = cnt == 6 && cnt1 == 1 ? 6
+                : cnt == 6 && cnt4 == 4 ? 9
+                : cnt == 5 && cnt1 == 2 ? 3
+                : cnt == 5 && cnt4 == 2 ? 2
+                : cnt == 5 ? 5 : val;
+            out = 10 * out + val;
+        }
+        res2 += out;
+    }
+    cout << "Part 1: " << res1 << endl;
+    cout << "Part 2: " << res2 << endl;
 }
 
 string input1() { return R"(

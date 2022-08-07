d278a4f7b70b0c84b8f7af8d892e6d251d4f2814
Alexander Zabluda <azabluda@gmail.com>
2022-08-07 19:53:16 +0200

    17_TrickShot part2

--- 17_TrickShot/Program.cs
+++ 17_TrickShot/Program.cs
@@ -1,9 +1,19 @@
 ﻿TrickShot(20, 30, -10, -5);
 TrickShot(94, 151, -156, -103);
 
-void TrickShot(int x0, int x1, int y0, int y1)
+void TrickShot(int left, int right, int bottom, int top)
 {
-    Console.WriteLine();
-    Console.WriteLine($"x={x0}..{x1}, y={y0}..{y1}");
-    Console.WriteLine($"Part 1: {y0 * (y0 + 1) / 2}");
+    // Part 1 :)
+    Console.WriteLine($"Part 1: {bottom * (bottom + 1) / 2}");
+
+    // Part 2
+    int miss = 0, hit = 1, hits = 0;
+    int Hit(int x, int y, int vx, int vy)
+        => x > right || y < bottom ? miss
+         : x >= left && y <= top ? hit
+         : Hit(x + vx, y + vy, vx - Math.Sign(vx), vy - 1);
+    for (int vx = 0; vx <= right; ++vx)
+        for (int vy = bottom; vy <= -bottom; ++vy)
+            hits += Hit(0, 0, vx, vy);
+    Console.WriteLine($"Part 2: {hits}");
 }
